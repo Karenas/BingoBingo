7e52caeea61171c861acf718601876d5c60ae683
guomeng <2002guomeng@163.com>
2019-05-05 10:59:58 +0800

    尝试编译aria2c，目前需要引入其它库

--- aria2c/src/main/cpp/aria2/Android.mk
+++ aria2c/src/main/cpp/aria2/Android.mk
@@ -1,4 +1,4 @@
-https://blog.csdn.net/qq_22494029/article/details/80972274
+
 ifneq ($(TARGET_SIMULATOR),true)
  include $(all-subdir-makefiles)
 # 如果用下述引入，提示 expat/Android.mk wslay/Android.mk aria2c-1.18.10/Android.mk 找不到。暂留作记录，仍使用上面的 include $(all-subdir-makefiles)
